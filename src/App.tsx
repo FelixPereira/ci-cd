0fd5ba2696cfd5a78c1ce4c2f9f5c7ea9feb94d3
FelixPereira <felixpereira538@gmail.com>
2023-04-23 13:08:48 +0100

    trigger

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Isto está muito rápido</h1>
+      <h1>Esta alterações é para o trigger do workflow</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
